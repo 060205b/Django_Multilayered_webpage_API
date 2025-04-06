083ef5f66894f78d62f6e8d2c021aab121c9ae08
Bhuvaneshwari balaji <146195186+060205b@users.noreply.github.com>
2025-04-06 18:47:13 +0530

    Update views.py

--- Multi_layered_upgraded/myapp/views.py
+++ Multi_layered_upgraded/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import UserSubmission  # Assuming you're storing form data
+from .models import UserSubmission
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def home(request):
     return render(request, 'myapp/index.html')
@@ -8,3 +10,11 @@ def home(request):
 def api_data(request):
     data = list(UserSubmission.objects.values())
     return JsonResponse(data, safe=False)
+
+@csrf_exempt
+def submit_vote(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        vote = data.get('vote_option')  # 'vote_option' should match the frontend
+        UserSubmission.objects.create(choice=vote)
+        return JsonResponse({'status': 'success'})
