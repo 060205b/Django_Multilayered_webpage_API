2658c84b7240ccca12304e8e91c216406a42952a
Bhuvaneshwari balaji <146195186+060205b@users.noreply.github.com>
2025-04-06 18:47:24 +0530

    Update urls.py

--- Multi_layered_upgraded/myapp/urls.py
+++ Multi_layered_upgraded/myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # for index.html
-    path('api/data/', views.api_data, name='api_data'),  # for API response
+    path('', views.home, name='index'),
+    path('api/data/', views.api_data, name='api_data'),
+    path('submit_vote/', views.submit_vote, name='submit_vote'),  # 👈 This one
 ]
