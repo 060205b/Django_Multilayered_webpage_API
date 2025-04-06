84504f031a0c61eecc5452502f3cfae2d07158cf
Bhuvaneshwari balaji <146195186+060205b@users.noreply.github.com>
2025-04-06 18:43:20 +0530

    Update urls.py

--- Multi_layered_upgraded/myapp/urls.py
+++ Multi_layered_upgraded/myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # or whatever your view is
+    path('', views.home, name='home'),  # for index.html
+    path('api/data/', views.api_data, name='api_data'),  # for API response
 ]
